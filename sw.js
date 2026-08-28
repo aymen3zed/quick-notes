68d887ed2305a299303a9a7c2dd8a21b5638c933
Aymen Zeghioud <149247842+aymen3zed@users.noreply.github.com>
2026-08-28 10:16:32 +0100

    add sw js

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE = "quick-notes-shell-v1";
+
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then(response => {
+          const responseClone = response.clone();
+
+          caches.open(CACHE).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+
+          return response;
+        })
+        .catch(() => {
+          return caches.match("./index.html");
+        });
+    })
+  );
+});
